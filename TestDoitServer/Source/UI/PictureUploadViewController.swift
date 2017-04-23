2dad74586c281f79ae0e8deb2651c1780ef7b03e
Andrew <shevtsov.ap@gmail.com>
2017-04-24 02:54:06 +0300

    PictureUploadViewController  keyboard handling

--- TestDoitServer/Source/UI/PictureUploadViewController.swift
+++ TestDoitServer/Source/UI/PictureUploadViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 import CoreLocation
 
-class PictureUploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PictureUploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    let statusBarHeight: CGFloat = 20
+    let navigationBarHeight: CGFloat = 44
+    
     @IBOutlet var chooseImageButton: UIButton?
     @IBOutlet var imageView: UIImageView?
     @IBOutlet var descriptionTextField: UITextField?
     @IBOutlet var tagTextField: UITextField?
+    @IBOutlet var topImageViewConstraint: NSLayoutConstraint?
+    @IBOutlet var topButtonConstraint: NSLayoutConstraint?
+    
+    var topConstraintConstant: CGFloat?
     
     lazy var pictureUploadMoldel: PictureUploadModel = {
         var model = PictureUploadModel()
@@ -54,6 +61,18 @@ class PictureUploadViewController: UIViewController, UIImagePickerControllerDele
                                                             action: #selector(PictureUploadViewController.uploadImage))
         
         chooseImageButtonTile = chooseImageButton?.title(for: .normal)
+        
+        if topConstraintConstant == nil {
+            topConstraintConstant = topImageViewConstraint?.constant
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeForKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        unsubscribeForKeyboardNotifications()
     }
     
     //MARK:- UIImagePickerControllerDelegate
@@ -71,6 +90,14 @@ class PictureUploadViewController: UIViewController, UIImagePickerControllerDele
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK:- UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
     }
     
     //MARK:- Actions
@@ -92,5 +119,68 @@ class PictureUploadViewController: UIViewController, UIImagePickerControllerDele
         }
         
         pictureUploadMoldel.upload()
+    }
+    
+    //MARK:- Keyboard Handling
+    
+    private func subscribeForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(LoginViewController.keyboardWillShow(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(LoginViewController.keyboardWillHide(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillHide,
+                                               object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo,
+            let field = tagTextField,
+//            let constraint1 = topImageViewConstraint,
+//            let constraint2 = topButtonConstraint,
+            let keyboardRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
+        {
+            let fieldBottomY = field.frame.maxY
+            let keyboardTopY = keyboardRect.minY
+            let delta = keyboardTopY - fieldBottomY - statusBarHeight - navigationBarHeight
+            if delta < 0 {
+                UIView.animate(withDuration: duration) { [unowned self] in
+                    self.topImageViewConstraint?.constant += delta
+                    self.topButtonConstraint?.constant += delta
+                    self.view.layoutIfNeeded()
+                }
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+//        if let constraint = centerYConstraint {
+//            if let userInfo = notification.userInfo,
+//                let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
+//            {
+//                UIView.animate(withDuration: duration, animations: {
+//                    constraint.constant = 0
+//                    self.view.layoutIfNeeded()
+//                })
+//            }
+//        }
+        if let constraint1 = topImageViewConstraint,
+            let constraint2 = topButtonConstraint,
+            let constant = topConstraintConstant
+        {
+            constraint1.constant = constant
+            constraint2.constant = constant
+            UIView.animate(withDuration: 0.3) { [unowned self] in
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    private func unsubscribeForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
     }
 }
